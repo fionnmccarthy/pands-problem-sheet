80d905b350e50e383813a06088afd6fa250fc1c9
Fionn McCarthy <G00301126@gmit.ie>
2021-02-09 23:07:05 +0000

    Week 03 task

---
+++ secondstring.py
@@ -0,0 +1,7 @@
+# secondstring.py
+# Weely Task 03
+# This program asks a user to input a string and outputs every second letter in reverse order. 
+# author: Fionn McCarthy
+
+inputString = input('Please enter a sentence:')
+print (inputString)
