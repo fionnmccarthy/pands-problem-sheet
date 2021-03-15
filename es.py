bbe193211661492234aab31a3e26799be22deabc
Fionn McCarthy <G00301126@gmit.ie>
2021-03-15 21:16:35 +0000

    Weekly Task 07

---
+++ es.py
@@ -0,0 +1,5 @@
+# es.py
+# Weekly Task 07.
+# Write a program that reads in a text file and outputs the number of e's it contains.
+# The program should take the filename from an argument on the command line.
+# author: Fionn McCarthy - G00301126.
