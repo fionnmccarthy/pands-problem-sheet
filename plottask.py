14f2d7e9bf411d4dc6c292138d136651504ea132
Fionn McCarthy <G00301126@gmit.ie>
2021-03-18 22:15:39 +0000

    Week 8 chnages

---
+++ plottask.py
@@ -0,0 +1,8 @@
+# plottask.py
+# Weekly Task 08.
+# Write a program called plottask.py that displays a plot of the functions: 
+# f(x)=x, g(x)=x2 and h(x)=x3 in the range [0, 4] on the one set of axes.
+# author: Fionn McCarthy - G00301126.
+
+import numpy as np
+import matplotlib.pyplot as plt
