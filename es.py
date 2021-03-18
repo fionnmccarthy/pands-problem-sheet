be1d8df3c65c280ac1778d17ffd1c5a822f881d8
Fionn McCarthy <G00301126@gmit.ie>
2021-03-18 19:10:33 +0000

    Code tidied up, references and comments

--- es.py
+++ es.py
@@ -1,5 +1,24 @@
+
 # es.py
 # Weekly Task 07.
 # Write a program that reads in a text file and outputs the number of e's it contains.
 # The program should take the filename from an argument on the command line.
 # author: Fionn McCarthy - G00301126.
+
+# As it was not speciifed I counted teh number of e's and E's within the text file. 
+
+filename = str(input("Please enter the name of the text file (must be *.txt file):")) # this line instructs teh user to input teh anme of the text file
+
+with open(filename, "r") as f:  #this line opens the file inputted (filename) in read mode ("r")
+    filedata = f.read() #read() reads the file and returns the specified number of bytes in the file 
+    letter_count = filedata.count("e") + filedata.count("E") #count() count sthe number of e's in the data that was read from the file
+    print(letter_count) 
+
+
+
+
+# References 
+# http://textfiles.com/stories/psf.txt text file source accessed on 11/03/2021
+# https://www.w3schools.com/python/ref_file_read.asp read() on 11/03/2021
+# https://www.w3schools.com/python/ref_list_count.asp count() on 11/03/2021
+
