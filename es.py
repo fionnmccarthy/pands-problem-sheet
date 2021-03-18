2c5e33a7bf0f282c26c6d4381866ccfb19cf5864
Fionn McCarthy <G00301126@gmit.ie>
2021-03-18 19:13:19 +0000

    Typos in text fixed

--- es.py
+++ es.py
@@ -1,20 +1,17 @@
-
 # es.py
 # Weekly Task 07.
 # Write a program that reads in a text file and outputs the number of e's it contains.
 # The program should take the filename from an argument on the command line.
 # author: Fionn McCarthy - G00301126.
 
-# As it was not speciifed I counted teh number of e's and E's within the text file. 
+# As it was not speciifed I counted the number of e's and E's within the text file. 
 
-filename = str(input("Please enter the name of the text file (must be *.txt file):")) # this line instructs teh user to input teh anme of the text file
+filename = str(input("Please enter the name of the text file:")) # this line instructs the user to input the name of the text file
 
 with open(filename, "r") as f:  #this line opens the file inputted (filename) in read mode ("r")
     filedata = f.read() #read() reads the file and returns the specified number of bytes in the file 
-    letter_count = filedata.count("e") + filedata.count("E") #count() count sthe number of e's in the data that was read from the file
+    letter_count = filedata.count("e") + filedata.count("E") #count() counts the number of e's in the data that was read from the file
     print(letter_count) 
-
-
 
 
 # References 
