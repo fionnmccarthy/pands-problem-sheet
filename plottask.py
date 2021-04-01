b834bd67c79b3795d64315b3092237c89740ceef
Fionn McCarthy <G00301126@gmit.ie>
2021-04-01 22:20:36 +0100

    Added in plots and uploading .png output file

--- plottask.py
+++ plottask.py
@@ -9,9 +9,30 @@ import matplotlib.pyplot as plt
 
 x = np.arange(0,4)
 
-fx = x 
+fx = x #f(x) = x
 
-gx = x*x
+gx = x**2 #g(x) = x^2
 
-hx = x*x*x
+hx = x**3 #h(x) = x^3
 
+plt.plot(fx, marker = 's', color = 'purple' , ls = ':', lw = '3', label = 'f(x)') #plot f(x), square marker, purple colour, linestyle is dotted, linewidth is 3, and label
+
+plt.plot(gx, marker = 's', color = 'red' , ls = ':', lw = '3', label = 'g(x)') #plot g(x), square marker, red colour, linestyle is dotted, linewidth is 3, and label
+
+plt.plot(hx, marker = 's', color = 'green' , ls = ':', lw = '3', label = 'h(x)') #plot h(x), square marker, green colour, linestyle is dotted, linewidth is 3, and label
+
+font1 = {'family':'serif','color':'darkblue','size':15} #setting font1 type
+font2 = {'family':'serif','color':'darkred','size':12} #setting font2 type
+
+plt.title("Plot of f(x)=x, g(x)=X^2 & h(x)=x^3 in the range[0,4]", fontdict = font1) #include plot title with font2
+plt.xlabel("X-axis", fontdict = font2) #include x-axis label with font2
+plt.ylabel("Y-axis", fontdict = font2) #include y-axis label with font2
+
+plt.grid(color = 'grey', ls = '--', lw = 0.5) #add grid lines to plot
+plt.legend() # include legend in plot
+plt.savefig('plottask_output.png') #save the plot output
+plt.show()
+
+
+
+# https://www.w3schools.com/python/matplotlib_intro.asp w3schools tutorial matplotlib 01/04/2021
